4b0d2245d1fd6e818c500a84e13dd81361c58bff
ryan <lrcrews@gmail.com>
2020-04-21 19:16:37 -0700

    update server.js config

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
-// Install express server
 const express = require('express');
 const app = express();
 const path = require('path');
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/build'));
 
-// Redirect direct URLs back to index so Angular may then route them properly
+// Redirect direct URLs back to index so React may then route them properly
 app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
+  res.sendFile(path.join(__dirname + '/build/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 3000);
