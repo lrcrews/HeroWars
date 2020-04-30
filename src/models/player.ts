ac308c5d1fa482e30aead38b3fdd0a322ff1b007
ryan <lrcrews@gmail.com>
2020-04-29 19:06:13 -0700

    [data entry] finish off data for war week 17 and remove level from data as it's current level and not level at the time (and level is typically not relevant)

--- src/models/player.ts
+++ src/models/player.ts
@@ -1,11 +1,11 @@
 export class Player {
   static fromJson(json: { [key: string]: any }): Player | undefined {
     if (json) {
-      return new Player(json['guild_id'], json['level'], json['name'], json['power']);
+      return new Player(json['guild_id'], json['name'], json['power']);
     } else {
       return undefined;
     }
   }
 
-  constructor(public guildId: number, public level: number, public name: string, public power: number) {}
+  constructor(public guildId: number, public name: string, public power: number) {}
 }
